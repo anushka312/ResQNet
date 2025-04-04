20626134a0aaa412d3283c0d9f6e839b241bc853
Anushka <anushka031205@gmail.com>
2025-04-04 20:15:03 +0530

    added app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login.jsx"
 import Report from './components/Report/Report.jsx'
 import Profile from './components/Profile/Profile.jsx'
+import Home from './components/Home.jsx';
+import Services from './components/Services.jsx';
+import Navbar from './components/Navbar.jsx';
 
 const App = () => {
   return (
     <Router> {/* Wrap everything in Router */}
       <div>
+        <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} /> 
           <Route path="/profile" element={<Profile />} /> 
           <Route path="/report" element={<Report />} />
+          <Route path="/report" element={<Services />} />
           
-          <Route path="/" element={<Profile />} />
+          
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
