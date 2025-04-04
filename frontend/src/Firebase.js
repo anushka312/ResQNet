d9660a9d41089f8c3bf595c88dfcd3757046d723
Anushka <anushka031205@gmail.com>
2025-04-05 03:42:08 +0530

    final

--- frontend/src/Firebase.js
+++ frontend/src/Firebase.js
@@ -7,13 +7,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_r1ki_r8VSPPXdznGQDNCHtGFKppV1Lw",
-  authDomain: "resqnet-9aa32.firebaseapp.com",
-  projectId: "resqnet-9aa32",
-  storageBucket: "resqnet-9aa32.firebasestorage.app",
-  messagingSenderId: "745201901073",
-  appId: "1:745201901073:web:ba34a276bf2316e7371bb0",
-  measurementId: "G-4VLB3LSVKP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
